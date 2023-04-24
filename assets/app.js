606fabec8a9d4934f26fa1abdea8183606d52454
ddimkou <ddimkou@outlook.com>
2023-04-25 02:25:49 +0300

    major changes
    +Added 3 more projects
    +Change navbar toggle
    + Changes in mobile/destop w/ lower screen

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,17 @@
 const hamburgerButton = document.querySelector("#hamburger");
-const navList = document.querySelector("#nav-list");
+// const navList = document.querySelector("#nav-list");
+const mobileMenu = document.querySelector(".nav-mobile");
 const toggleButton = () => {
-  navList.classList.toggle("show");
+  hamburgerButton.classList.toggle("is-active");
+  // navList.classList.toggle("show");
+  mobileMenu.classList.toggle("is-active");
 };
 hamburgerButton.addEventListener("click", toggleButton);
+
+const mobileLinks = document.querySelectorAll(".nav-mobile a");
+mobileLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    hamburgerButton.classList.remove("is-active");
+    mobileMenu.classList.remove("is-active");
+  });
+});
