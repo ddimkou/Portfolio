0cebbef1d7fdfd58e5d811b14b9cdd40514ab2ba
ddimkou <ddimkou@outlook.com>
2023-04-25 07:05:20 +0300

    visual changes

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,7 @@
 const hamburgerButton = document.querySelector("#hamburger");
-// const navList = document.querySelector("#nav-list");
 const mobileMenu = document.querySelector(".nav-mobile");
 const toggleButton = () => {
   hamburgerButton.classList.toggle("is-active");
-  // navList.classList.toggle("show");
   mobileMenu.classList.toggle("is-active");
 };
 hamburgerButton.addEventListener("click", toggleButton);
